61183705d13d9ff76aba0d18692c2782535019d6
Daniel Ferraz <danielferrazmm@hotmail.com>
2023-03-10 03:16:28 -0300

    desenvolvimento-js-web

---
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,15 @@
+function tocaSom(idAudio) {
+    document.querySelector(idAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+for (let i=0; i < listaDeTeclas.length; i++) {
+    const tecla = listaDeTeclas[i]
+    const instrumento = tecla.classList[1]
+    const idAudio = `#som_${instrumento}`;
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+}
